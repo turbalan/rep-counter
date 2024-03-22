2c33a904e4db09f547411de8b4500b7e7b33ac2e
Pavel Kuts <pavkuts@gmail.com>
2024-03-22 16:02:26 +0200

    Add a way to show/hide `TrainingLog`

--- src/components/TrainingLog/TrainingLog.js
+++ src/components/TrainingLog/TrainingLog.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { WorkoutsContext } from '../WorkoutProvider';
 
 function TrainingLog() {
+  const [showTrainingLog, setShowTrainingLog] = React.useState(false);
   const { workoutStatus, setWorkoutStatus, STATUS, getStoredTrainingLog } = React.useContext(WorkoutsContext);
   const storedTrainings = getStoredTrainingLog()
-  if (workoutStatus === STATUS.complete) return (
+
+  if (storedTrainings.length === 0) return;
+
+  if (!showTrainingLog) {
+    return <button onClick={() => setShowTrainingLog(true)}>Show Training Log</button>
+  } else { return (
     <div>
-      {storedTrainings.map(training => {
-        return (
-          <div key={training.date}>
-            <h2>{training.title}</h2>
-            <ul>
+    {storedTrainings.map(training => {
+      return (
+        <div key={training.date}>
+          <h2>{training.title}</h2>
+          <ul>
             {training.exercises.map((exercise, index) => {
               return (
                 <li key={index}>
-                  {exercise.exercise}, {exercise.sets} sets of {exercise.weight} kg for {exercise.reps} reps
+                {exercise.exercise}, {exercise.sets} sets of {exercise.weight} kg for {exercise.reps} reps
                 </li>
               )
             })}
-            </ul>
-          </div>
-        )
-      })}
+          </ul>
+        </div>
+      )
+    })}
+    <button onClick={() => {setShowTrainingLog(false)}}>Hide Training Log</button>
     </div>
   )
-    
+  }
 }
 
 export default TrainingLog;
